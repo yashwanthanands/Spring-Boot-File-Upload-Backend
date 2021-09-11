84f13f728a4c0a8e3cc60ef51f51171bdbcf830e
Yashwanth Anand S <yashwanthanands@outlook.com>
2021-09-11 17:00:30 +0530

    Commit with Initial Template from Spring Initializer

---
+++ src/main/java/com/yash/dev/fub/SpringBootFileUploadBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yash.dev.fub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFileUploadBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFileUploadBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yash/dev/fub/SpringBootFileUploadBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.dev.fub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFileUploadBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
