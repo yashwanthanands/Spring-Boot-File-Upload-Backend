619999a14f907989dbd8c444483df2649d42289b
Yashwanth Anand S <yashwanthanands@outlook.com>
2021-09-12 12:15:03 +0530

    Commit with File Upload Logic Backend

---
+++ src/main/java/com/yash/dev/fub/exception/FileUploadExceptionAdvice.java
@@ -0,0 +1,23 @@
+package com.yash.dev.fub.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.yash.dev.fub.message.ResponseMessage;
+
+/**
+ * Created by Yashwanth Anand S
+ **/
+
+@ControllerAdvice
+public class FileUploadExceptionAdvice extends ResponseEntityExceptionHandler {
+
+  @ExceptionHandler(MaxUploadSizeExceededException.class)
+  public ResponseEntity<ResponseMessage> handleMaxSizeException(MaxUploadSizeExceededException exc) {
+    return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage("File too large!"));
+  }
+}
